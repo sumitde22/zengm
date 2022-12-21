ade48acac1e8dde86c7ca1fd0bf4af63ad6e5f80
sumitde22 <39414740+sumitde22@users.noreply.github.com>
2022-12-21 13:49:50 -0500

    define winPToPick

--- src/worker/core/trade/buildFairTrade.ts
+++ src/worker/core/trade/buildFairTrade.ts
@@ -19,4 +19,15 @@ const buildFairTrade = () => {
 	refreshCache();
 };
 
+// Source: https://stackoverflow.com/questions/55725139/fit-sigmoid-function-s-shape-curve-to-data-using-python
+// This curve models specifically a mapping from win % to estimated draft pick based on simulation of 30 BBGM seasons
+// Different curves could be generated for other sports as well, and the parameters below could be decided based on sport
+const winPToPick = (winP: number) => {
+	const L = 1.0687820005007198;
+	const x0 = 0.4878517508315021;
+	const k = 10.626956987806935;
+	const b = -0.038756647038824504;
+	return g.get("numActiveTeams") * (L / (1 + Math.exp(-k * (winP - x0))) + b);
+};
+
 export default buildFairTrade;
