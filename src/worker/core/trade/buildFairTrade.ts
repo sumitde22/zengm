42d422a1a2da9c49885c21157a69f8eed78fae74
sumitde22 <39414740+sumitde22@users.noreply.github.com>
2022-12-21 13:44:46 -0500

    start cache

--- src/worker/core/trade/buildFairTrade.ts
+++ src/worker/core/trade/buildFairTrade.ts
@@ -1,3 +1,19 @@
-const buildFairTrade = () => {};
+import type { Result } from "regression";
+import type { TradePickValues } from "src/common/types";
+
+let cache: {
+	cacheKey: number;
+	ovrToRankModel: Result;
+	estValues: TradePickValues;
+};
+
+const refreshCache = () => {
+	if (cache === undefined) {
+	}
+};
+
+const buildFairTrade = () => {
+	refreshCache();
+};
 
 export default buildFairTrade;
